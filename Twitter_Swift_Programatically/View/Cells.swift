03c3581f900b4741f036ced95b1424082312ed01
masato <masato@m-mbp.local>
2018-10-18 17:06:58 +0800

    Photo & Name & Message

--- Twitter_Swift_Programatically/View/Cells.swift
+++ Twitter_Swift_Programatically/View/Cells.swift
@@ -22,6 +22,9 @@ class UserHeader: DatasourceCell {
     }
 }
 
+
+
+
 class UserCell: DatasourceCell {
 
     override var datasourceItem: Any? {
@@ -30,42 +33,66 @@ class UserCell: DatasourceCell {
         }
     }
 
+
     let profileImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .red
+        imageView.image = #imageLiteral(resourceName: "A4")
+
+        // Photo's CornerRadius
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+
         return imageView
     }()
 
+
     let nameLabel: UILabel = {
         let label = UILabel()
-        label.text = "TEST TEST TEST"
-        label.backgroundColor = .green
+        label.text = "Brian Voong"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
         return label
     }()
 
     let usernameLabel: UILabel = {
         let label = UILabel()
-        label.text = "username"
-        label.backgroundColor = .purple
+        label.text = "@buildthatapp"
+        label.font = UIFont.systemFont(ofSize: 14)
         return label
+    }()
+
+    let bioTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "iPhone, iPad, iOS Programming Community. Join us to learn Swift, Objective-C and build iOS apps!"
+        textView.font = UIFont.systemFont(ofSize: 15)
+        return textView
+    }()
+
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .cyan
+        return button
     }()
 
     override func setupViews() {
         super.setupViews()
-        backgroundColor = .yellow
 
         addSubview(profileImageView)
         addSubview(nameLabel)
         addSubview(usernameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
 
 
         profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
 
 
-
         // Text Position in Cell
-        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: self.rightAnchor, topConstant: 0, leftConstant: 4, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
 
-        usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+
+        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+
+        followButton.anchor(topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
     }
 }
