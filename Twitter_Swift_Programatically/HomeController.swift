d892d87a2715dabb3c8f6e1bc77cfd2da7fff2de
masato <masato@m-mbp.local>
2018-10-18 10:47:31 +0800

    Blue Cell

--- Twitter_Swift_Programatically/HomeController.swift
+++ Twitter_Swift_Programatically/HomeController.swift
@@ -8,11 +8,59 @@
 
 import UIKit
 
-class HomeController: UICollectionViewController {
+class WordCell: UICollectionViewCell {
+
+    // this gets called when
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+
+    let worldLabel: UILabel = {
+        let label = UILabel()
+        label.text = "TEST TEST TEST"
+        return label
+    }()
+
+    func setupViews() {
+        backgroundColor = .yellow
+
+        addSubview(worldLabel)
+        worldLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        worldLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        worldLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        worldLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    let cellId = "cellId"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .green
+        collectionView?.backgroundColor = .white
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
         
         }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .blue
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
 }
